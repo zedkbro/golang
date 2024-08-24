001b80b6f65581dbf8fe9665b211d85ccb991885
zinabuk <kalayuzinabu@gmail.com>
2024-08-24 18:49:20 +0300

    If else

--- hello.go
+++ hello.go
@@ -11,7 +11,7 @@ func main(){
     fmt.Printf("%T\n ", lastName)
     interns  := []string{"Zinabu", "Tsegay","Daniel"};
     fmt.Println(interns)
-
+    //  mySlice5 := append(interns, )
     // slice literal
     var mySlice = [3] int {1,2,3}
     fmt.Println(mySlice)
@@ -20,11 +20,21 @@ func main(){
     var makeSlice = make([]int , 4,10)
      
     // an array with defined length can't be a slice
-    fmt.Println(append(mySlice2, 4,5))
+    // var mySlice3 = [6] int {1, 2, 3, 4, 5, 6}
+
+    fmt.Println(append(mySlice2, makeSlice...))
     fmt.Println("Capacity of slice ",cap(makeSlice))
     fmt.Println(mySlice2)
     x := 3
+    var str string = ""
 
+    if x > 3 {
+         str = "Hi"
+    }else{
+        str = "Hello"
+    }
+
+    fmt.Println(str)
     switch x {
     case 1:
         fmt.Println("One")
@@ -37,7 +47,7 @@ func main(){
     var  num = 0
     fmt.Println("Enter input")
     fmt.Scanln(&num)
-
+ 
     fmt.Println(num)
     hello()
      }
